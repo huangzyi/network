ea41d0a8dbb0de987077d7afdedbb1e3d913c37f
huangzyi <huangzyi@163.com>
2016-03-25 01:11:01 +0800

    firsttime
    红岩网校第二次作业

---
+++ socket.php
@@ -0,0 +1,94 @@
+<?php
+/*
+$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);// 创建一个套接字（通讯节点）
+socket_connect($sock,"192.168.253.1", 10000);//开启一个套接字连接
+socket_set_option($sock, SOL_SOCKET, SO_BROADCAST, 1);//Sets socket options for the socket
+$buf = "Hello World!";
+socket_write($sock,$buf,strlen($buf));
+socket_close($sock);*/
+
+ini_set('error_reporting', E_ALL ^ E_NOTICE);
+ini_set('display_errors', 1);
+
+// Set time limit to indefinite execution
+set_time_limit (0);
+
+// Set the ip and port we will listen on
+//$address = '172.33.15.10';
+$address = '127.0.0.1';
+$port = "10000";
+
+// Create a TCP Stream socket
+$sock = socket_create(AF_INET, SOCK_STREAM, 0);
+
+// Bind the socket to an address/port
+socket_bind($sock, $address,$port) or die('Could not bind to address');
+
+// Start listening for connections
+socket_listen($sock);
+
+// Non block socket type
+socket_set_nonblock($sock);
+
+// Loop continuously
+while (true)
+{
+    unset($read);
+
+    $j = 0;
+
+    if (count($client))
+    {
+        foreach ($client AS $k => $v)
+        {
+            $read[$j] = $v;
+
+            $j++;
+        }
+    }
+
+    $client = $read;
+
+    if ($newsock = socket_accept($sock))
+    {
+        if (is_resource($newsock))
+        {
+            socket_write($newsock, "$j>", 2).chr(0);
+           
+            echo "New client connected $j";
+
+            $client[$j] = $newsock;
+
+            $j++;
+        }
+    }
+
+    if (count($client))
+    {
+        foreach ($client AS $k => $v)
+        {
+            if (@socket_recv($v, $string, 1024, MSG_DONTWAIT) === 0)
+            {
+                unset($client[$k]);
+
+                socket_close($v);
+            }
+            else
+            {
+                if ($string)
+                {
+                    echo "$k: $string\n";
+                }
+            }
+        }
+    }
+
+    //echo ".";
+
+    sleep(1);
+}
+
+// Close the master sockets
+socket_close($sock);
+
+?>
